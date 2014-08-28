202d23e54a07b30d5ed43afed0cefede885903df
Wangbarros <wangbarros@gmail.com>
2014-08-27 23:36:58 -0300

    Arrumou pequenos erros

--- Week1Algorithm.py
+++ Week1Algorithm.py
@@ -5,16 +5,16 @@ Spyder Editor
 This is a temporary script file.
 """
 
-EX_GRAPH0 = {0: [1,2], 1:[], 2:[]}
-EX_GRAPH1 = {0: [1,4,5], 1: [2,6], 2: [3], 3: [0], 4:[1], 5:[2], 6:[]}
-EX_GRAPH2 = {0: [1,4,5], 1: [2,6], 2: [3,7], 3: [7], 4:[1], 5:[2], 6:[], 7:[3], 8:[1,2], 9:[4,5,6,7]}
+EX_GRAPH0 = {0: set([1,2]), 1:set([]), 2:set([])}
+EX_GRAPH1 = {0: set([1,4,5]), 1: set([2,6]), 2: set([3]), 3: set([0]), 4:set([1]), 5:set([2]), 6:set([])}
+EX_GRAPH2 = {0: set([1,4,5]), 1: set([2,6]), 2: set([3,7]), 3: set([7]), 4:set([1]), 5:set([2]), 6:set([]), 7:set([3]), 8:set([1,2]), 9:set([0,3,4,5,6,7])}
 
 def make_complete_graph(num_nodes):
     complete = {}
     for i in range(0, num_nodes):
         lista = range(num_nodes)
         lista.remove(i)
-        complete[i] = lista
+        complete[i] = set(lista)
     print complete
     return complete    
 def compute_in_degrees(digraph):
